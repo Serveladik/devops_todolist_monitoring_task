5f4b8a1ee26090b95c1051d695614059c6cdf904
Serveladus <serveladik@gmail.com>
2024-08-16 14:15:51 +0300

    Solution

---
+++ src/todolist/view.py
@@ -0,0 +1,12 @@
+GET_REQUESTS = Counter("django_http_get_requests_total", "Total GET HTTP requests")
+POST_REQUESTS = Counter("django_http_post_requests_total", "Total POST HTTP requests")
+
+
+def metrics(request):
+    if request.method == "GET":
+        GET_REQUESTS.inc()
+    elif request.method == "POST":
+        POST_REQUESTS.inc()
+
+    metrics_data = generate_latest()
+    return HttpResponse(metrics_data, content_type="text/plain")
